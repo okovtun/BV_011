63c823fc5f458d7eec1efdc2b862c3f40671b57c
Clayman <Clayman@DESKTOP-649SK3N>
2020-11-02 21:01:02 +0200

    Geometria1

--- ControlStructures/Geometria/main.cpp
+++ ControlStructures/Geometria/main.cpp
@@ -1,17 +1,107 @@
 #include<iostream>
 using namespace std;
 
+//#define SQUARE
+//#define TRIANLE_1
+//#define TRIANLE_2
+//#define TRIANLE_3
+//#define TRIANLE_4
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	cout << "Введите число: "; cin >> n;
-	for (int i = 0; i < n; i++)//Повторяет вывод строки на экран, 'n' раз.
+	cout << "Введите размер фигуры: "; cin >> n;
+#ifdef SQUARE
+	for (int i = 0; i < n; i++)	//Повторяет вывод строки на экран. i - номер строки
 	{
-		for (int j = i; j < n; j++)//Повторяет вывод одной звездочки 'n' раз.
+		for (int j = 0; j < n; j++)//Повторяет вывод звездочки на экран. j считает длину строки (звездочки в строке)
 		{
 			cout << "* ";
 		}
+		cout << endl;
+	}
+#endif // SQUARE
+
+#ifdef TRIANLE_1
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // DEBUG
+
+#ifdef TRIANLE_2
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANLE_2
+
+#ifdef TRIANLE_3
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "  ";
+		}
+		for (int j = i; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANLE_3
+
+#ifdef TRIANLE_4
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n - 1; j++)
+		{
+			cout << "  ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANLE_4
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			cout << " ";
+		}
+		cout << "/";
+		for (int j = 0; j < i * 2; j++)
+		{
+			cout << " ";
+		}
+		cout << "\\";
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << " ";
+		}
+		cout << "\\";
+		//for (int j = 0; j < (n - i - 1) * 2; j++)
+		for (int j = (i+1)*2; j < (n) * 2; j++)
+		{
+			cout << " ";
+		}
+		cout << "/";
 		cout << endl;
 	}
 }
