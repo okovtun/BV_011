e3b76fd37b0fca5ed55f3d150c0b1f8c8b5713e3
Clayman <Clayman@DESKTOP-DM8E77U>
2021-01-13 19:58:16 +0200

    Separation DONE

---
+++ Functions/Arrays/Functions.cpp
@@ -0,0 +1,31 @@
+#include"Functions.h"
+
+//		Function definitions:
+
+int Sum(int arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+double Sum(double arr[], const int n)
+{
+	double Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+double Avg(double arr[], const int n)
+{
+	return Sum(arr, n) / n;
+}
