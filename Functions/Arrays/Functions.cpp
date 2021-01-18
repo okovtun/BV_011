29f80d0a06cb737e0825b500c242e2c734f219cb
Clayman <Clayman@DESKTOP-DM8E77U>
2021-01-18 20:25:18 +0200

    Separation DONE

---
+++ Functions/Arrays/Functions.cpp
@@ -0,0 +1,102 @@
+#include"Functions.h"
+
+
+template<typename T>void Print(T arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+template<typename T>void Print(T arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+template<typename T>void Sort(T arr[], const int n)
+{
+	//Сортировка одномерного массива:
+	for (int i = 0; i < n; i++)	//Выбирает элемент, в который нужно поместить минимальное значение
+	{
+		for (int j = i + 1; j < n; j++)	//Перебирает элементы, в поисках минимального значения
+		{
+			if (arr[j] < arr[i])
+			{
+				/*arr[i] ^= arr[j];
+				arr[j] ^= arr[i];
+				arr[i] ^= arr[j];*/
+				T buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+template<typename T>void Sort(T arr[ROWS][COLS], const int m, const int n)
+{
+#ifdef DEBUG
+	int iterations = 0;
+	int exchanges = 0;
+#endif // DEBUG
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = i; k < m; k++)
+			{
+				/*for (;;)
+				{
+
+				}*/
+
+				int l;
+				if (k == i)l = j + 1;
+				else l = 0;
+
+				//			condition ? value1 : value2;
+				for (/*int l = (k == i) ? j + 1 : 0*/; l < n; l++)
+				{
+#ifdef DEBUG
+					iterations++;
+#endif // DEBUG
+					if (/*ПЕРЕБИРАЕМЫЙ ЭЛЕМЕН*/arr[k][l] < arr[i][j]/*ВЫБРАННЫЙ ЭЛЕМЕН*/)
+					{
+						T buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+#ifdef DEBUG
+						exchanges++;
+#endif // DEBUG
+					}
+				}
+			}
+		}
+	}
+#ifdef DEBUG
+	cout << "Массив отсортирован за " << iterations << " итераций\n";
+	cout << "При этом, было выполнено " << exchanges << " обменов элементов\n";
+#endif // DEBUG
+}
+template<typename T>T Sum(T arr[], const int n)
+{
+	T Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+template<typename T>double Avg(T arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
