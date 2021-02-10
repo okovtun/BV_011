382b818f2a8efdfb441feb90bcea4827f989c5b8
Clayman <Clayman@DESKTOP-DM8E77U>
2021-02-10 21:49:29 +0200

    DynamicMemory day 1

---
+++ Pointers/References/main.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+//Ссылка(Reference) - это переменная,
+//которая содержит адрес другой переменной.
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;	//reference to 'a' - ссылка на 'a'
+	//& - показывает, что объявляемая переменная - это ссылка.
+	ra += 3;
+	cout << &a << endl;
+	cout << &ra << endl;
+}
